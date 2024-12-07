567cd97ba7430a336f9fd11e4f4ea0e3609c3a5d
Janani Chakrapani <39048979+cjanani@users.noreply.github.com>
2024-12-07 21:18:15 +1100

    Make CSS changes

--- src/main.js
+++ src/main.js
@@ -28,6 +28,12 @@ function display(obj)
     //console.log("Inside display", obj.title);
     let divq = document.getElementById('title');
     divq.innerHTML = `${obj.title} <br>`;
+    
+    // const titleLink = document.createElement('a');
+    // titleLink.href = `'http://google.com/search?q='+${obj.title}`;
+    // titleLink.target = "blank";
+    // divq.appendChild(titleLink);
+
     let divs = document.getElementById('desc');
     divs.innerHTML = ` ${obj.description} <br>`;
     let divp = document.getElementById('product');
@@ -35,8 +41,8 @@ function display(obj)
     let divt = document.getElementById('price');
     divt.innerHTML = `$${obj.price} <br>`;
     let divu = document.getElementById('rating');
-    divu.innerHTML = `<svg width="15px" height="15px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M923.2 429.6H608l-97.6-304-97.6 304H97.6l256 185.6L256 917.6l256-187.2 256 187.2-100.8-302.4z" fill="#FAD97F" /><path d="M1024 396H633.6L512 21.6 390.4 396H0l315.2 230.4-121.6 374.4L512 770.4l316.8 232L707.2 628 1024 396zM512 730.4l-256 187.2 97.6-302.4-256-185.6h315.2l97.6-304 97.6 304h315.2l-256 185.6L768 917.6l-256-187.2z" fill="" /></svg>`
-    divu.innerHTML += `${obj.rating}`;
+    divu.innerHTML = `<svg width="15px" height="15px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M923.2 429.6H608l-97.6-304-97.6 304H97.6l256 185.6L256 917.6l256-187.2 256 187.2-100.8-302.4z" fill="#FAD97F" /><path d="M1024 396H633.6L512 21.6 390.4 396H0l315.2 230.4-121.6 374.4L512 770.4l316.8 232L707.2 628 1024 396zM512 730.4l-256 187.2 97.6-302.4-256-185.6h315.2l97.6-304 97.6 304h315.2l-256 185.6L768 917.6l-256-187.2z" fill="" /></svg>${obj.rating}`
+    // divu.innerHTML += `${obj.rating}`;
     //width="400" 
 }
 function next()
